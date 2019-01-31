0cab798612f13977f00970ba47e6c8c004eeedd8
triciadang <c20tricia.dang@usafa.edu>
2019-01-31 09:19:31 -0800

    init

--- pex1.c
+++ pex1.c
@@ -1,7 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
 /*=============================================================================
  |   Assignment:  PEX1
- |       Author:
- |      Section:
+ |       Author:  Tricia Dang
+ |      Section:  M3
  |        Class:  CS483
  +-----------------------------------------------------------------------------
  |   Description:  DESCRIBE THE PROBLEM THAT THIS PROGRAM WAS WRITTEN TO
@@ -17,5 +20,35 @@
  *===========================================================================*/
 
 int main() {
+
+    char buf[50];
+    char* cmd_ptr_array[20];
+
+    printf("Enter command: ");
+    fgets(buf, 49, stdin);
+    buf[strlen(buf)-1] = '\0';
+
+    char* tmp_ptr;
+
+    while ( buf != "exit") {
+
+        tmp_ptr = strtok(buf, " \t");
+        int i = 0;
+
+        while (tmp_ptr != NULL) {
+            cmd_ptr_array[i = strdup(tmp_ptr)];
+            printf("token[%d]  \"%s\"\n", i, cmd_ptr_array[i]);
+            tmp_ptr = strtok(NULL, " \t");
+            i++;
+
+        }
+
+        cmd_ptr_array[i] = NULL;
+
+
+        printf("Enter command: ");
+        fgets(buf, 49, stdin);
+    }
+
     return 0;
 }
