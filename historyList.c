8d8aa0f74f0a17ca1123a25515733c51118225b9
triciadang <c20tricia.dang@usafa.edu>
2019-02-23 11:32:23 -0800

    got listtest to work and cd and exit need to do history and recall

--- historyList.c
+++ historyList.c
@@ -1,2 +1,234 @@
 // Your implementation of the functions defined in historyList.h goes here
+#include <malloc.h>
+#include "historyList.h"
 
+/* listInsertTail: create a new node at the end of the linked list
+   Parameters
+   - HistoryNodeType*: a pointer to the head of a linked list
+   - char*: data to be stored in the new node
+   Return: a pointer to the head of the linked list */
+HistoryNodeType* listInsertTail(HistoryNodeType* headNode, char* data){
+    HistoryNodeType* temp_ptr = headNode;
+    HistoryNodeType* new_node = malloc(sizeof(HistoryNodeType));
+    new_node->data = data;
+    HistoryNodeType* prev_ptr = NULL;
+
+    if (headNode == NULL){
+        headNode = new_node;
+    }
+
+    else {
+        while (temp_ptr != NULL) {
+            prev_ptr = temp_ptr;
+            temp_ptr = temp_ptr->next;
+        }
+
+        prev_ptr->next = new_node;
+    }
+
+    return headNode;
+
+}
+
+/* listInsertHead: create a new node and insert it at the head of the linked
+    list
+   Parameters
+   - HistoryNodeType*: a pointer to the head of a linked list
+   - char*: data to be stored in the new node
+   Return: a pointer to the new head of the linked list */
+HistoryNodeType* listInsertHead(HistoryNodeType* headNode, char* data){
+    HistoryNodeType* new_node = malloc(sizeof(HistoryNodeType));
+    new_node->data = data;
+
+    //if empty list
+    if (headNode == NULL){
+        headNode = new_node;
+        return headNode;
+    }
+
+    else{
+        new_node->next = headNode;
+        return new_node;
+    }
+}
+
+/* listInsertN: create a new node and insert it at the <n>th position,
+    where the head is at position 1
+   Parameters
+   - HistoryNodeType*: a pointer to the head of a linked list
+   - char*: data to be stored in the new node
+   - int: the position in which to insert the new node
+   Return: a pointer to the head of the linked list */
+HistoryNodeType* listInsertN(HistoryNodeType* headNode, char* data, int position){
+    HistoryNodeType* temp_ptr = headNode;
+    HistoryNodeType* new_node = malloc(sizeof(HistoryNodeType));
+    new_node->data = data;
+    HistoryNodeType* prev_ptr = NULL;
+
+    int curPos = 1;
+
+    //if want to insert into head
+    if (position == 1){
+        return listInsertHead(headNode, data);
+    }
+
+        //insert inside list
+    else {
+
+        while (temp_ptr != NULL && curPos < position) {
+            prev_ptr = temp_ptr;
+            temp_ptr = temp_ptr->next;
+            curPos++;
+        }
+
+        prev_ptr->next = new_node;
+        new_node->next = temp_ptr;
+    }
+
+    return headNode;
+
+
+}
+
+/* listRemove: remove the node containing specific data; if multiple nodes
+    contain the data, remove the node nearest to the head
+   Parameters
+   - HistoryNodeType*: a pointer to the head of a linked list
+   - char*: data that, if found in one of the nodes in the list, indicates
+      the node to remove (in the case of multiple nodes containing the data,
+	  remove the node nearest to the head)
+   Return: a pointer to the head of the linked list */
+HistoryNodeType* listRemove(HistoryNodeType* headNode, char* data){
+
+    int removedNode = 0;
+    HistoryNodeType* temp_ptr = headNode;
+    HistoryNodeType* prev_ptr = NULL;
+    int curPos = 1;
+
+    while (removedNode == 0){
+        if (temp_ptr->data == data){
+
+
+            //if removing the head
+            if (curPos == 1){
+                prev_ptr = headNode;
+                temp_ptr = prev_ptr->next;
+
+                free(prev_ptr);
+                return  temp_ptr;
+            }
+            else {
+                prev_ptr->next = temp_ptr->next;
+                free(temp_ptr);
+                removedNode = 1;
+            }
+
+        }
+        else{
+            prev_ptr = temp_ptr;
+            temp_ptr = temp_ptr->next;
+            curPos++;
+        }
+    }
+
+    return headNode;
+}
+
+/* listRemoveN: remove the <n>th node in the list
+   Parameters
+   - HistoryNodeType*: a pointer to the head of a linked list
+   - int: the position of the node to remove, where the head is at position 1
+   Return: a pointer to the head of the linked list */
+HistoryNodeType* listRemoveN(HistoryNodeType* headNode, int position){
+
+    HistoryNodeType* temp_ptr = headNode;
+    HistoryNodeType* prev_ptr = headNode;
+    int curPos = 1;
+
+    //if want to remove head
+    if (position == 1) {
+        prev_ptr = headNode;
+        temp_ptr = temp_ptr->next;
+
+        free(prev_ptr);
+        return  temp_ptr;
+    }
+
+    while (temp_ptr != NULL && curPos < position) {
+        prev_ptr = temp_ptr;
+        temp_ptr = temp_ptr->next;
+        curPos++;
+    }
+
+    prev_ptr->next = temp_ptr->next;
+    free(temp_ptr);
+
+    return headNode;
+
+}
+
+
+/* listPrint: print the data contained in each node in the linked list,
+    starting with the head, where each node's data is printed on a separate
+    line
+   Parameter: node*: a pointer to the head of a linked list */
+void listPrint(HistoryNodeType* headNode){
+
+    HistoryNodeType* temp_ptr = headNode;
+
+    while (temp_ptr != NULL){
+        printf("%s\n", temp_ptr->data);
+        temp_ptr = temp_ptr->next;
+
+    }
+
+}
+
+/* listprintN: print the data contained in the <n>th node of the list
+   Parameters:
+   - HistoryNodeType*: a pointer to the head of a linked list
+   - int: the position of the node with the data to print, where the head is
+       at position 1 */
+void listPrintN(HistoryNodeType* headNode, int position){
+    int curPos = 1;
+    HistoryNodeType* temp_ptr = headNode;
+
+    while (curPos < position){
+        temp_ptr = temp_ptr->next;
+        curPos++;
+    }
+    printf("%s", temp_ptr->data);
+
+}
+
+/* listGet: retrieve the data stored in the <n>th node in the list
+   Parameters:
+   - HistoryNodeType*: a pointer to the head of a linked list
+   - int: the position of the node with the data to retrieve, where the head is
+       at position 1
+   Return: the data */
+char* listGet(HistoryNodeType* headNode, int position){
+    int curPos = 1;
+    HistoryNodeType* temp_ptr = headNode;
+
+    while (curPos < position){
+        temp_ptr = temp_ptr->next;
+        curPos++;
+    }
+    return temp_ptr->data;
+
+}
+
+/* listDestroy: free all malloc'd memory used in the linked list,
+    including char*'s and HistoryNodeType*'s
+   Parameter: HistoryNodeType*: a pointer to the head of a linked list */
+void listDestroy(HistoryNodeType* headNode){
+    HistoryNodeType* free_ptr = NULL;
+    HistoryNodeType* temp_ptr = headNode;
+
+    while(temp_ptr!= NULL){
+        free_ptr = temp_ptr;
+        temp_ptr = temp_ptr->next;
+        free(free_ptr);
+    }
+}
--- pex1.c
+++ pex1.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include "historyList.h"
 
 /*=============================================================================
  |   Assignment:  PEX1
@@ -21,23 +25,104 @@
 
 int main() {
 
-    char buf[50];
+    char cwd[50];
+    char input_buf[50];
     char* cmd_ptr_array[20];
 
-    printf("Enter command: ");
-    fgets(buf, 49, stdin);
-    buf[strlen(buf)-1] = '\0';
+    int numberOfCommands = 0;
+    HistoryNodeType* historyList = malloc(sizeof(HistoryNodeType));
+
+    size_t size;
+
+    printf("%s",getcwd(cwd, size));
+    printf("> ");
+    fgets(input_buf, 49, stdin);
+    input_buf[strlen(input_buf)-1] = '\0';
 
     char* tmp_ptr;
 
-    while ( buf != "exit") {
+    tmp_ptr = strtok(input_buf, " \t");
+    int i = 0;
 
-        tmp_ptr = strtok(buf, " \t");
+    while (tmp_ptr != NULL) {
+        cmd_ptr_array[i] = strdup(tmp_ptr);
+//        printf("token[%d]  \"%s\"\n", i, cmd_ptr_array[i]);
+        tmp_ptr = strtok(NULL, " \t");
+        i++;
+
+    }
+
+    cmd_ptr_array[i] = NULL;
+
+    while (strcmp(cmd_ptr_array[0], "exit")!=0){
+
+        if (strcmp(cmd_ptr_array[0], "history")==0) {
+//            for (int i = atoi(cmd_ptr_array[1]); i < 1; i-- ){
+//                printf("%d %s", i, listGet(historyList, i));
+//
+//            }
+
+
+        }
+
+        else if (strcmp(cmd_ptr_array[0], "recall")==0) {
+        }
+        else if (strcmp(cmd_ptr_array[0], "cd")==0) {
+            if (strcmp(cmd_ptr_array[1], "~") == 0){
+                chdir(getenv("HOME"));
+            }
+
+            else{
+                chdir(cmd_ptr_array[1]);
+            }
+
+//            printf("%s", cmd_ptr_array[1]);
+
+        }
+
+
+            //external command
+        else{
+            pid_t ret_val;
+            int status;
+
+            ret_val = fork();
+            if (ret_val != 0){
+                wait(&status);
+            }
+            else{
+                execvp(cmd_ptr_array[0], cmd_ptr_array);
+
+            }
+
+        }
+
+        //if it is your first command
+        if (numberOfCommands == 0){
+            listInsertHead(historyList, cmd_ptr_array);
+            numberOfCommands++;
+        }
+        else{
+            if (strcmp(cmd_ptr_array, listGet(historyList, numberOfCommands)) != 0){
+                listInsertTail(historyList, cmd_ptr_array);
+                numberOfCommands++;
+            }
+        }
+
+
+        printf(getcwd(cwd, size));
+        printf("> ");
+        fgets(input_buf, 49, stdin);
+        input_buf[strlen(input_buf)-1] = '\0';
+
+        char* tmp_ptr;
+
+        tmp_ptr = strtok(input_buf, " \t");
         int i = 0;
 
         while (tmp_ptr != NULL) {
-            cmd_ptr_array[i = strdup(tmp_ptr)];
-            printf("token[%d]  \"%s\"\n", i, cmd_ptr_array[i]);
+            cmd_ptr_array[i] = strdup(tmp_ptr);
+//            printf("token[%d]  \"%s\"\n", i, cmd_ptr_array[i]);
             tmp_ptr = strtok(NULL, " \t");
             i++;
 
@@ -45,10 +130,11 @@ int main() {
 
         cmd_ptr_array[i] = NULL;
 
-
-        printf("Enter command: ");
-        fgets(buf, 49, stdin);
     }
 
+    free(historyList);
+
+
     return 0;
+
 }
